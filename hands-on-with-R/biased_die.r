dda2b84951a7d063ae4ab9b636719abb846935b2
Usmaan Ali <usmaanalii@hotmail.co.uk>
2016-11-16 21:25:06 +0000

    Add files via upload

---
+++ hands-on-with-R/biased_die.r
@@ -0,0 +1,12 @@
+roll <- function() {
+  die <- 1:6
+  names(die) <- c("one", "two", "three", "four", "five", "six")
+  dice <- sample(die, size = 2, replace = TRUE, prob = c(1/8, 1/8, 1/8, 1/8, 1/8, 3/8))
+  sum(dice)
+}
+
+
+rolls <- replicate(10000, roll())
+qplot(rolls, binwidth = 1)
+
+?matrix
