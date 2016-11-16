365c67f84cb2fe955a20afc0d87e2856c7fd12f1
Usmaan Ali <usmaanalii@hotmail.co.uk>
2016-11-16 16:14:14 +0000

    Update t-test.r

--- class-work/t-test.r
+++ class-work/t-test.r
@@ -1,28 +1,30 @@
 sample_1 = c(79.98, 80.04, 80.02, 80.04, 80.03, 80.03, 80.04, 79.97, 80.05, 80.03, 80.02, 80.00, 80.02)
 sample_2 = c(80.02, 79.94, 79.98, 79.97, 79.97, 80.03, 79.95, 79.97)
 
-t_test <- function(A, B) {
+t_test <- function(s1, s2) {
   
-  sample_1_length <- length(A)
-  sample_1_mean <- mean(A)
-  sample_1_var <- var(A)
+  s1_length <- length(s1)
+  s1_mean <- mean(s1)
+  s1_var <- var(s1)
   
-  sample_2_length <- length(B)
-  sample_2_mean <- mean(B)
-  sample_2_var <- var(B)
+  s2_length <- length(s2)
+  s2_mean <- mean(s2)
+  s2_var <- var(s2)
   
-  pooled_var <- sqrt(((sample_1_length - 1) * sample_1_var + 
-                       (sample_2_length - 1) * sample_2_var) /
-                       (sample_1_length + sample_2_length - 2)
-                       )
+  pooled_var <- sqrt( ( (s1_length - 1) * s1_var + (s2_length - 1) * s2_var ) /
+                       (s1_length + s2_length - 2)
+                    )
   
-  result = (sample_1_mean - sample_2_mean) / 
-    (pooled_var * sqrt(1/(sample_1_length) + 1/(sample_2_length)))
+  result = (s1_mean - s2_mean) / 
+    ( pooled_var * sqrt( 1 / (s1_length) + 1 / (s2_length) )
+    )
   
   print(result)
 }
 
+# result
 
+t_test(sample_1, sample_2)
 
 
 
