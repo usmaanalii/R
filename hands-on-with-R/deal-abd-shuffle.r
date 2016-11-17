587e4ae47ccbd7462c0c92c8d41a47e66dae8064
Usmaan Ali <usmaanalii@hotmail.co.uk>
2016-11-17 20:25:32 +0000

    Create deal-abd-shuffle.r

---
+++ hands-on-with-R/deal-abd-shuffle.r
@@ -0,0 +1,30 @@
+# Dealing cards by extracting rows
+deal <- function(cards) {
+  cards[1, ]
+}
+
+# Selecting different values from the cards deck
+deck2 <- cards[1:52, ]
+head(deck2)
+
+deck3 <- cards[c(2, 1, 3:52), ]
+head(deck3)
+
+# Shuffle cards by using retrieving rows at random
+shuffle <- function(cards) {
+  random <- sample(1:52, size = 52)
+  new_deck <- cards[random, ]
+  new_deck
+}
+
+# Testing the shuffle function
+deal(cards) # card A
+
+new_deck <- shuffle(cards)
+
+deal(new_deck) # card B
+
+# dollar sign
+cards$value
+mean(cards$value)
+median(cards$value)
