246edf1956ab7a528344bcf8e01340ad7e137074
Usmaan Ali <usmaanalii@hotmail.co.uk>
2016-11-18 23:27:38 +0000

    Add files via upload

---
+++ hands-on-with-R/blackjack.r
@@ -0,0 +1,17 @@
+setup <- function(deck) {
+  DECK <- deck
+  
+  DEAL <- function() {
+    card <- deck[1, ]
+    assign("deck", deck[-1, ], envir = parent.env(environment()))
+    card
+  }
+  
+  SHUFFLE <- function() {
+    random <- sample(1:52, size = 52)
+    assign("deck", DECK[random, ], envir = parent.env(environment()))
+  }
+  
+  list(deal = DEAL, shuffle = SHUFFLE)
+}
+
